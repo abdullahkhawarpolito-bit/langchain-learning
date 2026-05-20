a5fa88a8f545d7cd9e5cd29fcceb939427943243
Abdullah Khawar <abdullahkhawarpolito@gmail.com>
2026-05-20 17:48:41 +0500

    Fix import statement for StrOutputParser

--- 06_langchain_runnables/runnable_branch.py
+++ 06_langchain_runnables/runnable_branch.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParse
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
 
