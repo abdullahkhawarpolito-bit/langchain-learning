18ae855b6e109e30591c9fd423660dab5c112318
Abdullah Khawar <abdullahkhawarpolito@gmail.com>
2026-05-20 17:48:16 +0500

    Fix import statement for PromptTemplate

--- 06_langchain_runnables/runnable_branch.py
+++ 06_langchain_runnables/runnable_branch.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplat
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough, RunnableBranch, RunnableLambda
